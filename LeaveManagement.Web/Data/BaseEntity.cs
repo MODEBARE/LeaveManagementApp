a8f3dfe35b8aa33e7cf4190895eb2efbec5e568e
MODEBARE <modebare20@gmail.com>
2023-03-06 17:34:22 +0100

    Interface functionality

---
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+    }
+}
